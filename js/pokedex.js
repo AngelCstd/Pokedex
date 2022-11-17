19934836a9b9f2244bd24fb7e5eed2fc9e3cf813
AngelCstd <luisacast.29@gmail.com>
2022-11-16 18:53:01 -0600

    falta poner info

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,31 +1,53 @@
-const buscarPokemon = async () => {
+async function buscarPokemon() {
+
     abrirPokedex();
+
+    mostrarInfo();
+
     const nombrePokemonInput = document.getElementById("nombrePokemon");
-    let nombrePokemon = nombrePokemonInput.value;
-nombrePokemon = nombrePokemon.toLowerCase();
-const url = "https://pokeapi.co/api/v2/pokemon/"+nombrePokemon
-let data = await fetch(url).then((res)=>{
-    if(res.status != "200"){
-        console.log(res);
+    let nombrePokemon = nombrePokemonInput.value.toLowerCase();
+    const url = "https://pokeapi.co/api/v2/pokemon/" + nombrePokemon
+    let data = await fetch(url).then((res) => {
+        if (res.status != "200") {
+            console.log(res);
+        } else {
+            return res.json();
+        }
+    });
+    if (data) {
+        pokeImg(data.sprites.other.home.front_default);
 
+    };
+}
+
+function abrirPokedex() {
+    const pokeAbrir = document.getElementById("check");
+    if (pokeAbrir.checked == true) {
+        pokeAbrir.click();
+        setTimeout(() => pokeAbrir.click(), 1000);
     }else{
-        return res.json();
+        pokeAbrir.click();
     }
-})
-if(data){
-    let imagenPokemon = data.sprites.other.home.front_default;
-    pokeImg(imagenPokemon)
-}
 }
 
-
-function pokeImg(url){
-const pokemonImagen = document.getElementById("imagenPokemon");
-pokemonImagen.src = url;
+function mostrarInfo() {
+    let pokeInfo = document.getElementById("main");
+    if (pokeInfo.style.display == 'grid') {
+        pokeInfo.style.display = 'none';
+        setTimeout(() => pokeInfo.style.display = 'grid', 1000);
+    }else{
+        setTimeout(() => pokeInfo.style.display = 'grid', 500);
+    }
 }
 
-
-function abrirPokedex(){
-    let abrirPokedex = document.getElementById("check");
-    abrirPokedex.click();
+function pokeImg(url) {
+    const pokemonImagen = document.getElementById("imagenPokemon");
+    pokemonImagen.src = url;
 }
+
+function pokeId(id, name) {
+
+    
+}
+
+document.getElementById("botonBuscar").onclick = () => buscarPokemon();
